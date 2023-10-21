153eb4882510809a9878f39cadd4600d2d98235b
santoshchappidi11 <santoshchappidi3@gmail.com>
2023-10-21 23:49:08 +0530

    updates

--- task-management/src/App.js
+++ task-management/src/App.js
@@ -1,7 +1,30 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import AddTask from "./Components/Task/AddTask";
+import CompletedTasks from "./Components/Task/CompletedTasks";
+import DeletedTasks from "./Components/Task/DeletedTasks";
+import EditTask from "./Components/Task/EditTask";
 
 function App() {
-  return <div className="App">TODO</div>;
+  return (
+    <div className="App">
+      TODO
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/add-task" element={<AddTask />} />
+        <Route exact path="/edit-task" element={<EditTask />} />
+        <Route exact path="/completed-tasks" element={<CompletedTasks />} />
+        <Route exact path="/deleted-tasks" element={<DeletedTasks />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ task-management/src/Components/Task/DeletedTasks.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DeletedTasks = () => {
+  return <div>DeletedTasks</div>;
+};
+
+export default DeletedTasks;
